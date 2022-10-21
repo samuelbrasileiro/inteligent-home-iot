9c9821a32c816e8c41d1322b1fbbb3c53c7108c5
Samuel <samuelbsantosn@gmail.com>
2022-10-21 14:04:23 -0300

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,9 +1,10 @@
 from typing import Union
 from datetime import datetime
-import matplotlib.pyplot as plt
-import matplotlib.dates
+#import matplotlib.pyplot as plt
+#import matplotlib.dates
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import firebase_admin
 from firebase_admin import db
@@ -16,10 +17,20 @@ class Log(BaseModel):
 
 cred_obj = firebase_admin.credentials.Certificate('credentials/serviceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
-	'databaseURL': 'https://smart-home-e8e9e-default-rtdb.firebaseio.com/'
-	})
+    'databaseURL': 'https://smart-home-e8e9e-default-rtdb.firebaseio.com/'
+})
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 ref = db.reference("/logs")
 
@@ -34,17 +45,20 @@ async def root():
     resistorKWs = ('resistorKW', [log['resistorKW'] for log in array])
     
     keyed = [resistorKWs, yellowKWs, greenKWs, redKWs]
-
-    dates = matplotlib.dates.date2num([datetime.fromtimestamp(log['timestamp']) for log in array])
+    dates = [datetime.fromtimestamp(log['timestamp']) for log in array]
+    #dates = matplotlib.dates.date2num([datetime.fromtimestamp(log['timestamp']) for log in array])
     
-    plt.xlabel('Tempo ->')
-    plt.ylabel('KW gasto ->')
-    for k in keyed:
-       plt.plot_date(dates, k[1], '-', label=k[0])
-    plt.legend()
-    print("dd")
-    plt.savefig('relatories/relatory'+ str(datetime.now()) +'.pdf')
-    print("ee")
+    #plt.xlabel('Tempo ->')
+    #plt.ylabel('KW gasto ->')
+    #for k in keyed:
+    #   plt.plot_date(dates, k[1], '-', label=k[0])
+    #plt.legend()
+    #plt.savefig('relatories/relatory'+ str(datetime.now()) +'.pdf')
+    print("Relatório")
+    for item in keyed:
+        print(item[0])
+        for i in range(len(item)):
+            print(str(dates[i]) + " - " + str(item[1][i]) )
     return {}
 
 @app.post("/logs/")
